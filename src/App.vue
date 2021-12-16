2c9dd60e5ffeb4ae1339c73906dfaba4000f63f1
Gabriela Almeida <gaby.lisboa284@gmail.com>
2021-12-16 12:12:30 +0000

    Pokemon evolutions API

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div></div>
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <HelloAPI />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloAPI.vue'
+import HelloAPI from './components/HelloAPI.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloAPI
   }
 }
 </script>
---
+++ src/components/HelloAPI.vue
@@ -0,0 +1,92 @@
+<template>
+  <h2 class="title"> Choose your initial Pokemon </h2>
+  <p class="hint"> When you choose your preferred Pokemon, you can see its evolutions. </p>
+
+  <pokemon-cards 
+    :pokemon="pokemon" 
+    :selectedId="selectedId"
+    @pokemonSelected="fetchEvolutions"
+  />
+
+  <pokemon-cards :pokemon="evolutions" />
+</template>
+
+<script>
+import PokemonCards from './PokemonCards.vue'
+
+const api = 'https://pokeapi.co/api/v2/pokemon'
+const ids = [1, 4, 7, 25]
+
+export default {
+  components: {
+    PokemonCards
+  },
+
+  data() {
+    return {
+      pokemon: [],
+      evolutions: [],
+      selectedId: null
+    }
+  },
+
+  async created() {
+    this.pokemon = await this.fetchData(ids)
+  },
+
+  methods: {
+    async fetchEvolutions(pokemon) {
+      if (pokemon.id !== 25) {
+        this.evolutions = await this.fetchData(
+          [pokemon.id + 1, pokemon.id + 2]
+        )
+      } else {
+      this.evolutions = await this.fetchData(
+          [pokemon.id + 1]
+        )
+      }
+      this.selectedId = pokemon.id
+    },
+
+    async fetchData(idsToFetch) {
+      const responses = await Promise.all(
+         idsToFetch.map(id => window.fetch(`${api}/${id}`))
+      )
+
+      const data = await Promise.all(
+        responses.map(response => response.json())
+      )
+
+      return data.map(datum => ({
+        id: datum.id,
+        name: datum.name,
+        sprite: datum.sprites.other['official-artwork'].front_default,
+        types: datum.types.map(type => {
+          return {
+            name: type.type.name
+          }
+        })
+      }))
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+}
+
+.cards {
+  display: flex;
+}
+
+.opace {
+  opacity: 0.5;
+}
+
+.card:hover {
+  opacity: 1;
+}
+
+</style>
